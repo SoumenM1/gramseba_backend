d1508ffbf6310063284b616dc577b697fabb47ab
soumen <soumen@idreameducation.org>
2024-10-04 10:40:41 +0530

    fix bugs

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,10 +5,10 @@ const errorHandler = (err, req, res, next) => {
     const message = err.message || 'Internal Server Error';
   
     // Log the error (optional, for debugging purposes)
-    console.error(err);
+    // console.error(err);
   
     // Send error response
-    res.status(statusCode).json({
+   return res.status(statusCode).json({
       success: false,
       message,
       stack: process.env.NODE_ENV === 'production' ? null : err.stack // Hide stack trace in production
