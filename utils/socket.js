ee441f2d3981dd595122afe26d5e7547c66b3ff3
soumen <soumen@idreameducation.org>
2024-10-21 10:43:21 +0530

    not conent socket

--- utils/socket.js
+++ utils/socket.js
@@ -5,9 +5,8 @@ module.exports = {
     io = require('socket.io')(server, {
       cors: {
         origin: "https://grambazer.gramsaba.in", // or use "*" for any origin
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Authorization"], // Specify allowed headers if needed
-        credentials: true, // Allow credentials (cookies, etc.)
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true, // Include credentials if needed
       },
     });
     return io;
