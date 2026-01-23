ebbc5cabc32eaa7d2a3245c178b6c4d9bdd61c0c
soumen <soumen@idreameducation.org>
2026-01-23 16:02:59 +0530

    add vercel

--- app.js
+++ app.js
@@ -60,8 +60,5 @@ io.on('connection', (socket) => {
 });
 
 // Server listening
-if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-module.exports = app;
