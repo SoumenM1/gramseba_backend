5e520e60725eac7eb485592f7d1e453ae7f28ec5
soumen <soumen@idreameducation.org>
2026-01-27 19:50:07 +0530

    fix eail send

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, name, otp, forget) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // must be false for 587
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // must exist in Render
